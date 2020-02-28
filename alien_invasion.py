8d4ed6f4845301b09bbb9b9cf1b01a3bbc595d81
liang8214 <liang8214@163.com>
2020-02-28 21:53:28 +0800

    外星人代码完成

--- alien_invasion.py
+++ alien_invasion.py
@@ -3,6 +3,7 @@ import pygame
 from pygame.sprite import Group
 from settings import Settings 
 from ship import Ship
+#from alien import Alien
 import game_functions as gf 
 
 
@@ -18,9 +19,14 @@ def run_game():
 	#创建一条飞船
 	ship = Ship(ai_settings,screen)
 
+
+
 	#创建一个用于存储子弹的编组
 	bullets = Group()
+	aliens = Group()
 
+	#创建外星人群
+	gf.create_fleet(ai_settings,screen,ship,aliens)
 
 	# 开始游戏主循环
 	while True:
@@ -29,11 +35,11 @@ def run_game():
 		gf.check_events(ai_settings,screen,ship,bullets) # 直接用模块调用函数
 		ship.update()
 		gf.update_bullets(bullets)
-	
-		gf.update_screen(ai_settings,screen,ship,bullets)
+		gf.update_aliens(ai_settings,aliens)
+		gf.update_screen(ai_settings,screen,ship,aliens,bullets)
                 
-
 		#让最近绘制的屏幕可见
 		pygame.display.flip()
+
 
 run_game()
